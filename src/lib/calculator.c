45a9240b08e4a5aabbdeab23729ef2cbe8573ab6
kamil-elm <kamilEM57@gmail.com>
2024-11-12 14:22:09 +0100

    calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,18 +1,26 @@
 #include "./../../include/calculator.h"
 
 double _add(double a, double b) {
-	return a+b;
+    return a + b;
 }
 
 double _sub(double a, double b) {
-	return a-b;
+    return a - b;
 }
 
 double _mul(double a, double b) {
-	return a*b;
+    return a * b;
 }
 
 double _div(double a, double b) {
-	return a/b;
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Erreur : division par zéro\n");
+        return 0;
+    }
 }
-	
+
+double _car(double a) {
+    return a * a;
+}
