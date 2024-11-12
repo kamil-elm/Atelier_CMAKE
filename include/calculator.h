d17f105386b5c2c30aa5c698446935319fb0fb3f
kamil-elm <kamilEM57@gmail.com>
2024-11-12 14:22:33 +0100

    calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,11 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+// Prototypes des fonctions
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+double _car(double a); // Prototype de la fonction car
+
+#endif
